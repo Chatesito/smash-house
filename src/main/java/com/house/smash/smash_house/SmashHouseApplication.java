dbdea6bbd732cc01f318fac39418564d5d92fa95
Chatesito <juandavidriveracrack@gmail.com>
2024-11-12 00:14:05 -0500

    Inicializar repositorio con archivos locales

---
+++ src/main/java/com/house/smash/smash_house/SmashHouseApplication.java
@@ -0,0 +1,13 @@
+package com.house.smash.smash_house;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmashHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmashHouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/house/smash/smash_house/SmashHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.house.smash.smash_house;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmashHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
